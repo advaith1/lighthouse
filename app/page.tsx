7aa77c0c75bb9c1bcbdd38b0e42cee9d3d8bc3d6
advaith <advaithj1@gmail.com>
2024-01-16 00:17:29 -0800

    lighthouse

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,66 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+
+import { useFormState, useFormStatus } from 'react-dom'
+import { submit } from './submit'
 
 export default function Home() {
+  const [state, formAction] = useFormState(submit, '')
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <section className="section">
+        <div className="container">
+          <h1 className="title">
+            Lighthouse
+          </h1>
+          <form action={formAction}>
+            <div className="field">
+              <label className="label">User</label>
+              <div className="control">
+                <input className="input" type="email" autoComplete="email" name="user" placeholder="sslug@ucsc.edu" required />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Autograder Password</label>
+              <div className="control">
+                <input className="input" type="password" autoComplete="current-email" name="password" required />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Course</label>
+              <div className="control">
+                <input className="input" type="text" placeholder="CSE40" name="course" required />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Assignment</label>
+              <div className="control">
+                <input className="input" type="text" placeholder="HO0" name="assignment" required />
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <SubmitButton />
+              </div>
+            </div>
+          </form>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
+      <section className="section">
+        <div className="container title is-1">
+          {state}
+        </div>
+      </section>
     </main>
+  )
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus()
+ 
+  return (
+    <button className={`button is-link ${pending ? 'is-loading' : ''}`}>
+      Get Score
+    </button>
   )
 }
---
+++ app/submit.ts
@@ -0,0 +1,32 @@
+'use server'
+
+async function sha256(message: string) {
+  const msgBuffer = new TextEncoder().encode(message);                    
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
+
+export async function submit(prevState: any, data: FormData) {
+  'use server'
+
+  const body = new FormData()
+  body.append('content', JSON.stringify({
+    'course-id': data.get('course'),
+    'user-email': data.get('user'),
+    'user-pass': await sha256(data.get('password') as string),
+    'assignment-id': data.get('assignment'),
+  }))
+
+  const res = await (await fetch('http://lighthouse.soe.ucsc.edu/api/v02/submission/peek', {
+    method: 'POST',
+    body
+  })).json()
+
+  if (!res.success) return `Error: ${res.message}`
+
+  if (!res.content['found-submission']) return "You haven't submitted this assignment yet!"
+
+  return `${res.content['submission-result'].score} / ${res.content['submission-result'].max_points}`
+}
